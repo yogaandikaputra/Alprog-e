91cc96db57462300283912ca13fe3ad19b5f4b93
yoga andika putra <andikaputrayoga139@gmail.com>
2024-11-17 20:29:44 +0800

    modul

---
+++ prog6.py
@@ -0,0 +1,25 @@
+# variabel assgment value
+x = 10
+y = 6
+print("nilai dari x adalah :", y)
+
+# aturan penamaan fariabel
+luas = 30 # bisa berupa kata
+yoga_ganteng = 100 # jika kata digabung dg "-"
+yogaX = 10 # boleh gabung capital non-capital
+yogaX = 3
+gopole7 = 10 # jika menggabungkan huruf dan angka,angkanya di akhir
+print("nilai dari yoga adalah : ", gopole7 )
+
+
+a = 4 # integer bilangan bulat
+print("tipe data dari a : ", type(a))
+
+b = 6.9 # float bilangan pecahan
+print("tipe dari data b : ", type (b))
+
+c = "gacor" # string, gabungan karakter
+print("tipe data dari c : ", type (c))
+
+d = False # boolean, true false 
+print("tipe dari d : ", type (d))
---
+++ progtugas4.py
@@ -0,0 +1,4 @@
+print ("nama saya yoga andika putra")
+print ("nim saya adalah 24241163")
+print ("saya berasal dari desa pengenjek")
+print ("87")
